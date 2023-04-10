af64c9e7d1503285a2441f06b71c1cea40147d14
Arthur Moser <arthur.moser@hotmail.com>
2023-04-10 14:59:24 -0300

    Task Scheduler and app.Config

--- trayApplication/rekapp-tray/Program.cs
+++ trayApplication/rekapp-tray/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using Microsoft.Win32.TaskScheduler;
 
 namespace rekapp_tray
 {
@@ -14,6 +16,28 @@ namespace rekapp_tray
         [STAThread]
         static void Main()
         {
+            var user_email = ConfigurationManager.AppSettings["email"];
+            var user_ip = ConfigurationManager.AppSettings["ip"];
+            string taskName = "Open Notepa++";
+            TaskDefinition td = TaskService.Instance.NewTask();
+            td.RegistrationInfo.Author = "Rekapp";
+            td.Actions.Add(new ExecAction(@"C:\Program Files\Notepad++\notepad++.exe"));
+
+            //TaskService.Instance.RootFolder.RegisterTaskDefinition(taskName, td).Run();
+            //folder.RegisterTaskDefinition(taskName, td).Run();
+
+            TaskService ts = TaskService.Instance;
+            var folder = TaskService.Instance.RootFolder.CreateFolder("Rekapp");
+            var tasks = ts.GetFolder("Rekapp").Tasks;
+
+            foreach (var task in tasks)
+            {
+                if (task.Name == taskName)
+                {
+                    task.Run();
+                }
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new App());
