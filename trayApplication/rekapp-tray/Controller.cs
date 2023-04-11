60406826fae8f3317a5d063532f9d5fd5ec12a38
Arthur Moser Silva <fs2118@t-systems.com.br>
2023-04-11 18:44:10 -0300

    Thread infita, e Request completo

--- trayApplication/rekapp-tray/Controller.cs
+++ trayApplication/rekapp-tray/Controller.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
 
 namespace rekapp_tray
 {
@@ -41,90 +38,5 @@ namespace rekapp_tray
                 Console.WriteLine("Error creating or writing to file: " + ex.Message);
             }
         }
-
-        public async void ListenRequest()
-        {
-            int port = 12345; // Port number to listen on
-
-            // Create a TcpListener bound to the specified IP address and port
-            TcpListener listener = new TcpListener(IPAddress.Any, port);
-
-            // Start listening for incoming connections
-            listener.Start();
-            Console.WriteLine($"Listening on port {port}...");
-
-            try
-            {
-                while (true)
-                {
-                    // Accept an incoming connection
-                    TcpClient client = await listener.AcceptTcpClientAsync();
-                    Console.WriteLine("Client connected.");
-
-                    // Get the network stream for reading and writing data
-                    NetworkStream stream = client.GetStream();
-
-                    // Read data from the client
-                    byte[] buffer = new byte[1024];
-                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
-                    string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                    Console.WriteLine($"Received data: {receivedData}");
-
-                    // Parse the received data as an HTTP request
-                    HttpRequest httpRequest = HttpRequest.Parse(receivedData);
-
-                    // Check if the request is a POST request
-                    if (httpRequest.Method == "POST")
-                    {
-                        // Extract the data from the POST request
-                        string postData = httpRequest.Body;
-
-                        // Process the POST data
-                        // ... (do something with postData)
-
-                        // Send a response back to the client
-                        string responseData = "Hello from the server!";
-                        byte[] responseBuffer = Encoding.UTF8.GetBytes(responseData);
-                        await stream.WriteAsync(responseBuffer, 0, responseBuffer.Length);
-                        Console.WriteLine($"Sent data: {responseData}");
-                    }
-
-                    // Close the client connection
-                    client.Close();
-                    Console.WriteLine("Client disconnected.");
-                }
-            }
-            finally
-            {
-                // Stop listening for connections
-                listener.Stop();
-            }
-        }
-
-        // Simple HTTP request class for parsing HTTP requests
-        public class HttpRequest
-        {
-            public string Method { get; set; }
-            public string Path { get; set; }
-            public string Body { get; set; }
-
-            public static HttpRequest Parse(string httpRequest)
-            {
-                HttpRequest request = new HttpRequest();
-                string[] lines = httpRequest.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-                string[] requestLine = lines[0].Split(' ');
-                request.Method = requestLine[0];
-                request.Path = requestLine[1];
-
-                // Extract the body from the request, if present
-                int byteIndex = Array.IndexOf(lines, "");
-                if (byteIndex != -1 && byteIndex + 1 < lines.Length)
-                {
-                    request.Body = lines[byteIndex + 1];
-                }
-                return request;
-            }
-        }
-
     }
 }
--- trayApplication/rekapp-tray/Program.cs
+++ trayApplication/rekapp-tray/Program.cs
@@ -1,58 +1,88 @@
-﻿using Microsoft.Win32.TaskScheduler;
-using System;
-using System.Configuration;
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
 using System.Windows.Forms;
+
 
 
 namespace rekapp_tray
 {
     static class Program
     {
-        /// <summary>
-        /// Ponto de entrada principal para o aplicativo.
-        /// </summary>
-        [STAThread]
+        static HttpListener listener;
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
         static void Main()
         {
+
+
 
             //JsonModel json = new JsonModel();
             //json.InactivityThreshold = "18.569";
             //control.CreateFile(json);
+            //var user_email = ConfigurationManager.AppSettings["email"];
+            //Console.WriteLine($"User {user_email}");
+            //string taskName = "Open Notepad++";
+            //TaskDefinition td = TaskService.Instance.NewTask();
+            //td.RegistrationInfo.Author = "Rekapp";
+            //td.Actions.Add(new ExecAction(@"C:\Program Files\Notepad++\notepad++.exe"));
+            //TaskService ts = TaskService.Instance;
+            ////var folder = TaskService.Instance.RootFolder.CreateFolder("Rekapp");
+            //var tasks = ts.GetFolder("Rekapp").Tasks;
 
-            var user_email = ConfigurationManager.AppSettings["email"];
-            Console.WriteLine($"User {user_email}");
 
-            string taskName = "Open Notepad++";
-            TaskDefinition td = TaskService.Instance.NewTask();
-            td.RegistrationInfo.Author = "Rekapp";
-            td.Actions.Add(new ExecAction(@"C:\Program Files\Notepad++\notepad++.exe"));
 
-            //TaskService.Instance.RootFolder.RegisterTaskDefinition(taskName, td).Run();
-            //folder.RegisterTaskDefinition(taskName, td).Run();
+            string url = "http://localhost:8080/"; // URL where the server will listen for incoming requests
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add(url);
+            listener.Start();
+            Console.WriteLine($"Server listening on {url}");
 
-            TaskService ts = TaskService.Instance;
-            //var folder = TaskService.Instance.RootFolder.CreateFolder("Rekapp");
-            var tasks = ts.GetFolder("Rekapp").Tasks;
 
-            foreach (var task in tasks)
+
+            // Start a separate thread to handle incoming requests
+            Thread thread = new Thread(() =>
             {
-                if (task.Name == taskName)
+                while (true)
                 {
-                    task.Run();
-                }
-            }
+                    HttpListenerContext context = listener.GetContext();
+                    HttpListenerRequest request = context.Request;
 
+
+
+                    // Read the request body data
+                    using (StreamReader reader = new StreamReader(request.InputStream, request.ContentEncoding))
+                    {
+                        string requestBody = reader.ReadToEnd();
+                        Console.WriteLine($"Received request with body: {requestBody}");
+                        // Process the request data as needed
+                    }
+
+
+
+                    // Send a response back to the client
+                    string responseString = "Hello from C# server!";
+                    byte[] responseBytes = System.Text.Encoding.UTF8.GetBytes(responseString);
+                    context.Response.ContentLength64 = responseBytes.Length;
+                    context.Response.OutputStream.Write(responseBytes, 0, responseBytes.Length);
+                    context.Response.OutputStream.Close();
+                }
+            });
+            thread.Start();
+
+
+
+            // Run the WinForms application
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new App());
-
-            // Create an instance of the Controller class
-            Controller controller = new Controller();
-
-            // Call the ListenRequest method to start listening for incoming requests
-            controller.ListenRequest();
-
-
         }
+        public static void Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            listener?.Stop(); // Stop the HttpListener
+        }
     }
 }
