4bf8423099fb89ae1bcd575e6ebce2a51e6c9b96
Arthur Moser <arthur.moser@hotmail.com>
2023-04-13 14:05:58 -0300

    TaskScheduler working, Listen Port working

--- trayApplication/rekapp-tray/Program.cs
+++ trayApplication/rekapp-tray/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32.TaskScheduler;
+using System;
 using System.IO;
 using System.Net;
 using System.Threading;
@@ -18,29 +19,11 @@ namespace rekapp_tray
         static void Main()
         {
 
-
-
-            //JsonModel json = new JsonModel();
-            //json.InactivityThreshold = "18.569";
-            //control.CreateFile(json);
-            //var user_email = ConfigurationManager.AppSettings["email"];
-            //Console.WriteLine($"User {user_email}");
-            //string taskName = "Open Notepad++";
-            //TaskDefinition td = TaskService.Instance.NewTask();
-            //td.RegistrationInfo.Author = "Rekapp";
-            //td.Actions.Add(new ExecAction(@"C:\Program Files\Notepad++\notepad++.exe"));
-            //TaskService ts = TaskService.Instance;
-            ////var folder = TaskService.Instance.RootFolder.CreateFolder("Rekapp");
-            //var tasks = ts.GetFolder("Rekapp").Tasks;
-
-
-
             string url = "http://localhost:8080/"; // URL where the server will listen for incoming requests
             HttpListener listener = new HttpListener();
             listener.Prefixes.Add(url);
             listener.Start();
             Console.WriteLine($"Server listening on {url}");
-
 
 
             // Start a separate thread to handle incoming requests
@@ -50,21 +33,19 @@ namespace rekapp_tray
                 {
                     HttpListenerContext context = listener.GetContext();
                     HttpListenerRequest request = context.Request;
-
-
-
+                    string requestText;
                     // Read the request body data
                     using (StreamReader reader = new StreamReader(request.InputStream, request.ContentEncoding))
                     {
                         string requestBody = reader.ReadToEnd();
+                        requestText = requestBody;
                         Console.WriteLine($"Received request with body: {requestBody}");
                         // Process the request data as needed
                     }
 
-
-
                     // Send a response back to the client
-                    string responseString = "Hello from C# server!";
+                    string responseString = $"Hello from C# server! {requestText} ";
+                    TaskSchedulerConf(requestText);
                     byte[] responseBytes = System.Text.Encoding.UTF8.GetBytes(responseString);
                     context.Response.ContentLength64 = responseBytes.Length;
                     context.Response.OutputStream.Write(responseBytes, 0, responseBytes.Length);
@@ -80,6 +61,32 @@ namespace rekapp_tray
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new App());
         }
+        public static void TaskSchedulerConf(string requestText)
+        {
+
+            string taskName = requestText;
+            TaskDefinition td = TaskService.Instance.NewTask();
+            td.RegistrationInfo.Author = "Rekapp Ltda.";
+            td.Actions.Add(new ExecAction(@"C:\Program Files\Notepad++\notepad++.exe"));
+
+            TaskService.Instance.RootFolder.RegisterTaskDefinition(taskName, td).Run();
+
+            //folder.RegisterTaskDefinition(taskName, td).Run();
+
+            //TaskService ts = TaskService.Instance;
+            //var folder = TaskService.Instance.RootFolder.CreateFolder("Rekapp");
+            //var tasks = ts.GetFolder("Rekapp").Tasks;
+
+            //foreach (var task in tasks)
+            //{
+            //    if (task.Name == taskName)
+            //    {
+            //        task.Run();
+            //    }
+            //}
+
+        }
+
         public static void Form_FormClosing(object sender, FormClosingEventArgs e)
         {
             listener?.Stop(); // Stop the HttpListener
